9d8050d87c743003583b5486de8adbb567b01f67
cheporte <cheporte.dev@gmail.com>
2025-05-13 00:48:12 +0300

    🍒 Feat: setup Go project and entry point

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("🌸 Welcome to Martha - your vault of code and memory.")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cheporte/martha
+
+go 1.24.2
